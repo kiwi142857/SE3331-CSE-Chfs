ec7b424de75033045d31ae7d2d9ac4f65f514560
Kiwi <779490624@qq.com>
2024-12-20 03:20:57 +0000

    format lab4

--- src/map_reduce/mr_coordinator.cc
+++ src/map_reduce/mr_coordinator.cc
@@ -1,41 +1,46 @@
-#include <string>
-#include <vector>
+#include <mutex>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <unistd.h>
-#include <mutex>
+#include <vector>
 
 #include "map_reduce/protocol.h"
 
-namespace mapReduce {
-    std::tuple<int, int> Coordinator::askTask(int) {
-        // Lab4 : Your code goes here.
-        // Free to change the type of return value.
-        return std::make_tuple(-1, -1);
-    }
-
-    int Coordinator::submitTask(int taskType, int index) {
-        // Lab4 : Your code goes here.
-        return 0;
-    }
-
-    // mr_coordinator calls Done() periodically to find out
-    // if the entire job has finished.
-    bool Coordinator::Done() {
-        std::unique_lock<std::mutex> uniqueLock(this->mtx);
-        return this->isFinished;
-    }
-
-    // create a Coordinator.
-    // nReduce is the number of reduce tasks to use.
-    Coordinator::Coordinator(MR_CoordinatorConfig config, const std::vector<std::string> &files, int nReduce) {
-        this->files = files;
-        this->isFinished = false;
-        // Lab4: Your code goes here (Optional).
-    
-        rpc_server = std::make_unique<chfs::RpcServer>(config.ip_address, config.port);
-        rpc_server->bind(ASK_TASK, [this](int i) { return this->askTask(i); });
-        rpc_server->bind(SUBMIT_TASK, [this](int taskType, int index) { return this->submitTask(taskType, index); });
-        rpc_server->run(true, 1);
-    }
+namespace mapReduce
+{
+std::tuple<int, int> Coordinator::askTask(int)
+{
+    // Lab4 : Your code goes here.
+    // Free to change the type of return value.
+    return std::make_tuple(-1, -1);
 }
+
+int Coordinator::submitTask(int taskType, int index)
+{
+    // Lab4 : Your code goes here.
+    return 0;
+}
+
+// mr_coordinator calls Done() periodically to find out
+// if the entire job has finished.
+bool Coordinator::Done()
+{
+    std::unique_lock<std::mutex> uniqueLock(this->mtx);
+    return this->isFinished;
+}
+
+// create a Coordinator.
+// nReduce is the number of reduce tasks to use.
+Coordinator::Coordinator(MR_CoordinatorConfig config, const std::vector<std::string> &files, int nReduce)
+{
+    this->files = files;
+    this->isFinished = false;
+    // Lab4: Your code goes here (Optional).
+
+    rpc_server = std::make_unique<chfs::RpcServer>(config.ip_address, config.port);
+    rpc_server->bind(ASK_TASK, [this](int i) { return this->askTask(i); });
+    rpc_server->bind(SUBMIT_TASK, [this](int taskType, int index) { return this->submitTask(taskType, index); });
+    rpc_server->run(true, 1);
+}
+} // namespace mapReduce
--- src/map_reduce/mr_worker.cc
+++ src/map_reduce/mr_worker.cc
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,41 +7,48 @@
 
 #include <mutex>
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 #include "map_reduce/protocol.h"
 
-namespace mapReduce {
+namespace mapReduce
+{
 
-    Worker::Worker(MR_CoordinatorConfig config) {
-        mr_client = std::make_unique<chfs::RpcClient>(config.ip_address, config.port, true);
-        outPutFile = config.resultFile;
-        chfs_client = config.client;
-        work_thread = std::make_unique<std::thread>(&Worker::doWork, this);
-        // Lab4: Your code goes here (Optional).
-    }
+Worker::Worker(MR_CoordinatorConfig config)
+{
+    mr_client = std::make_unique<chfs::RpcClient>(config.ip_address, config.port, true);
+    outPutFile = config.resultFile;
+    chfs_client = config.client;
+    work_thread = std::make_unique<std::thread>(&Worker::doWork, this);
+    // Lab4: Your code goes here (Optional).
+}
 
-    void Worker::doMap(int index, const std::string &filename) {
+void Worker::doMap(int index, const std::string &filename)
+{
+    // Lab4: Your code goes here.
+}
+
+void Worker::doReduce(int index, int nfiles)
+{
+    // Lab4: Your code goes here.
+}
+
+void Worker::doSubmit(mr_tasktype taskType, int index)
+{
+    // Lab4: Your code goes here.
+}
+
+void Worker::stop()
+{
+    shouldStop = true;
+    work_thread->join();
+}
+
+void Worker::doWork()
+{
+    while (!shouldStop) {
         // Lab4: Your code goes here.
-    }
-
-    void Worker::doReduce(int index, int nfiles) {
-        // Lab4: Your code goes here.
-    }
-
-    void Worker::doSubmit(mr_tasktype taskType, int index) {
-        // Lab4: Your code goes here.
-    }
-
-    void Worker::stop() {
-        shouldStop = true;
-        work_thread->join();
-    }
-
-    void Worker::doWork() {
-        while (!shouldStop) {
-            // Lab4: Your code goes here.
-        }
     }
 }
+} // namespace mapReduce
